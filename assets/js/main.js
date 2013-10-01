b2816fa7c8ee855d8e8d5bdd78d67626afa20a42
Bryan M. Robles <bryanaka0@gmail.com>
2013-10-01 12:02:46 -0700

    working on navigation some more.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
+(function($, window, document, undefined){
+	'use strict';
 
+	$(function(){
+		$('.mainNav').on('click', '.mainNav__link--dropdown', function(e) {
+			var $this = $(this);
+			if(e.target && e.target.className === 'subNav__link') {
+				e.stopPropagation();
+				return;
+			}
+			e.preventDefault();
+			
+			if(!$this.closest('.mainNav__linkGroup').hasClass('selected')) {
+				$('.mainNav__linkGroup').removeClass('selected');
+				$this.closest('.mainNav__linkGroup').toggleClass('selected');
+			} else {
+				$('.mainNav__linkGroup').removeClass('selected');
+			}
+		});
+
+		$('body').on('click', function(e) {
+			// close the nav menu when clicking elsewhere
+		});
+	});
+})(jQuery, window, document);
